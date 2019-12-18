535d5c307bbb3bd60736b048bf02e831eb884f78
YuGYAO <ygy851934017@qq.com>
2019-12-18 19:31:56 +0800

    debug

--- my-app/src/ajax/index.ts
+++
@@ -1,4 +0,0 @@
-// 提供axios
-
-import HttpRequest from './request'
-export default new HttpRequest();
--- my-app/src/pages/common/login/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login:React.FC<{}>=function Login(){
-  return (
-    <div>登录</div>
-  )
-}
-
-export default Login;
